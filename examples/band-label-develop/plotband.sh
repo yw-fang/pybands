fa3148dac84b7f1f4975c35969e26a84f639c0e0
yw-fang <fyuewen@gmail.com>
2020-02-15 09:02:34 +0900

    plot script, shell

---
+++ examples/band-label-develop/plotband.sh
@@ -0,0 +1,27 @@
+# ##Er-d projecte
+# python ./pyband-largefont-whitebackground \
+# 	--occ '1 2 3 4' --spd '4 5 6 7 8' -k gxsygzurtzytuxsr  \
+# 	--dpi '300' --occMc 'lawngreen' --occMs '1000' --lw '3.5'  \
+# 	--size 20 6 -y -2.0 4.0 --lsorbit \
+#         --output 'band-Er-d-SOC.png'
+# ##Ag-d projected
+# python ./pyband-largefont-whitebackground \
+# 	--occ '5 6 7 8' \
+# 	--spd '4 5 6 7 8' -k gxsygzurtzytuxsr  --dpi '300' \
+# 	--occMc 'orange' --occMs '1000' --lw '3.5' \
+# 	--size 20 6 -y -2.0 4.0 --lsorbit \
+#         --output 'band-Ag-d-SOC.png' 
+# ##Se-p projected
+# python ./pyband-largefont-whitebackground \
+# 	--occ '9 10 11 12 13 14 15 16' \
+# 	--spd '1 2 3' -k gxsygzurtzytuxsr  --dpi '300' \
+# 	--occMc 'orange' --occMs '1000' --lw '3.5' \
+# 	--size 20 6 -y -2.0 4.0 --lsorbit \
+#         --output 'band-Se-p-SOC.png' 
+## No orbital projection   gxsygzurtz|yt|ux|sr 
+python ./pyband-largefont-whitebackground -k g-x1-s-y-g-z-u-r-t-z\|y-t\|u-x\|s-r  --dpi '72' \
+	--lw '3.5' \
+	--size 20 6 -y -2. 4. --lsorbit --output 'band.pdf' --quiet 
+# python ./pyband-largefont --occ '2 3' --occL
+
+
